8194c0c26af4edb98cdb8ec06332423a64018cfb
SRsoz <sozyar.rauf@chasacademy.se>
2025-10-22 19:28:54 +0200

    cleared app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
-import SearchBar from './components/Searchbar';
 function App() {
 
 
   return (
-    <main className="min-h-screen flex items-center justify-center bg-[#f9f9f9]">
-      <SearchBar />
+    <main> 
     </main>
   );
 }
