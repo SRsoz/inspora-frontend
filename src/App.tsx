7e4318a5d70e140989fef07bda7b7bb2f852612a
Sozyar Rauf <sozyar.rauf@chasacademy.se>
2025-10-25 16:05:42 +0200

    Remove Form component
    Removed the Form component and its usage from App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css';
-import Form from './components/Form'; // importera din Form-komponent
 
 function App() {
   return (
     <div>
-      {/* Visa Sign In-formuläret */}
-      <Form mode="signin" />
-
-      {/* Om du vill testa Sign Up-formuläret istället, byt mode:
-      <Form mode="signup" /> 
-      */}
     </div>
   );
 }
