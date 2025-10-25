7de14ceae82ff5ef3976158e657b202cf6bd6a9b
SRsoz <sozyar.rauf@chasacademy.se>
2025-10-25 16:02:27 +0200

    form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import './App.css'
+import './App.css';
+import Form from './components/Form'; // importera din Form-komponent
+
 function App() {
   return (
     <div>
+      {/* Visa Sign In-formuläret */}
+      <Form mode="signin" />
+
+      {/* Om du vill testa Sign Up-formuläret istället, byt mode:
+      <Form mode="signup" /> 
+      */}
     </div>
   );
 }
 
-export default App
+export default App;
